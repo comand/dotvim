f169673808ce4ab64603f106bffbe46fb80feb4b
Cory Omand <coryomand@gmail.com>
2014-08-28 10:40:09 -0700

    Clone neobundle first if it does not exist.

--- update-bundles.sh
+++ update-bundles.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+NEOBUNDLEDIR=~/.vim/bundle/neobundle.vim
+if [[ ! -d $NEOBUNDLEDIR ]]; then
+    git clone https://github.com/Shougo/neobundle.vim $NEOBUNDLEDIR
+fi
+
 vim -R -N -V1 -e -s -i NONE \
     --cmd "source ~/.vimrc" \
     --cmd "NeoBundleFetch 'Shugo/neobundle.vim'" \
