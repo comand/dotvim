79b837afb7fef539fe3c7c6a59bcbb8711926e27
Cory Omand <coryomand@gmail.com>
2015-01-23 10:37:55 -0800

    Script that wraps startup profiling.

---
+++ profile-startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+exec gvim \
+    --cmd 'profile start profile.log' \
+    --cmd 'profile func *' \
+    --cmd 'profile file *' \
+    -c 'profdel func *' \
+    -c 'profdel file *' \
+    -c 'qa!' \
+    "$@"
