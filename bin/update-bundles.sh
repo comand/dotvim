fda98556e4e7dd18e56659ad340bbbfc90c5dd69
Cory Omand <coryomand@gmail.com>
2017-02-02 14:19:41 -0800

    Remove neobundle installer.

--- bin/update-bundles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-NEOBUNDLEDIR=~/.vim/bundle/neobundle.vim
-
-if [[ ! -d $NEOBUNDLEDIR ]]; then
-    git clone https://github.com/Shougo/neobundle.vim $NEOBUNDLEDIR
-fi
-
-$NEOBUNDLEDIR/bin/neoinstall
--- bin/update.sh
+++ bin/update.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
 vim -c ":PlugUpdate"
